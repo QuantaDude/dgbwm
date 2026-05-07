bac21e73e65913ee2219d86d0b54148876b7405e
Abhirup Bhattacharyya <quantadude@zohomail.in>
2026-05-07 09:59:13 +0530

    Modified emacs configuration
    - Added treemacs and keybinds.
    - Added Vertico completion.
    - Improved dired, changing its defaults.
    - Added keybinds to move cursor between the buffer splits.
    - Unset C-h for opening help
    
    	modified:   .config/emacs/config.org
    	modified:   .config/emacs/early-init.el

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,1 +1,12 @@
+;; Remove help from C-h
+(keymap-global-unset "C-h")
+
+(global-set-key (kbd "C-h") #'windmove-left)
+(global-set-key (kbd "C-l") #'windmove-right)
+(global-set-key (kbd "C-k") #'windmove-up)
+(global-set-key (kbd "C-j") #'windmove-down)
+
+
+(global-set-key (kbd "C-c h") #'help-command)
+
 (setq package-enable-at-startup nil)
